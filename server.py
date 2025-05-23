d833783d592410ec105a1d08eca8b567e3ea3c04
Tom Myers <tmyers273@gmail.com>
2025-05-23 15:23:57 +0100

    Fix

--- server.py
+++ server.py
@@ -446,7 +446,7 @@ async def ingest_na_sp_traffic(limit: int = 5):
         print(
             f"    Inserted {df.height} rows at {rate:.2f}k rows/s (total rate: {total_rate:.2f}k rows/s)"
         )
-        shutil.move(file, os.path.join("/done", file))
+        shutil.move(file, f"/done{file}")
 
     return {"message": "Table created"}
 
